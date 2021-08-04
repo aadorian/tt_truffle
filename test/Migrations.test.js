58e828df3261744da6b01ae4844d655128b19792
aadorian <aadorian@gmail.com>
2021-08-04 19:43:11 +0000

    add:describe test

--- test/Migrations.test.js
+++ test/Migrations.test.js
@@ -1,16 +1,20 @@
 const { expect } = require('chai');
 
 const Contract = artifacts.require('Migrations');
- 
+
+describe("#Migration", function () {
+
 contract('Migration', function () {
   beforeEach(async function () {
     this.contract = await Contract.new();
   });
- 
+
   it('retrieve returns a value previously stored', async function () {
     let value = 10
     let expected_value = '10' 
     await this.contract.set(value);
     expect((await this.contract.get()).toString()).to.equal(expected_value);
   });
+  
+});
 });
